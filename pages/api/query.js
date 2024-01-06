c6474ad21fe8d12dec2ec2a2f3e42215807f04c4
M. Sertaç Toröz <msertactoroz@gmail.com>
2024-01-07 01:25:31 +0300

    first

---
+++ pages/api/query.js
@@ -0,0 +1,50 @@
+import xml2js from 'xml2js';
+
+export const Query = {
+    async getQuery() {
+        try {
+            const response = await fetch('https://www.apkmirror.com/apk/instagram/instagram-instagram/feed/');
+            const xml = await response.text();
+
+            // Parse XML to JSON
+            const json = await new Promise((resolve, reject) => {
+                xml2js.parseString(xml, { explicitArray: false }, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            });
+
+            // Extract relevant information from each item
+            const extractedItems = json.rss.channel.item.map(item => {
+                const extractedData = {
+                    title: item.title,
+                    link: item.link,
+                    pubDate: item.pubDate,
+                    description: item.description,
+                    // Add more properties as needed
+                };
+
+                return extractedData;
+            });
+
+            console.log(extractedItems);
+            return extractedItems;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+};
+
+export default async function handler(req, res) {
+    try {
+        const data = await Query.getQuery();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+
+export default function Home() {
+  const [query, setQuery] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch('http://localhost:3000/api/query');
+      const data = await res.json();
+
+      if (Array.isArray(data)) {
+        setQuery(data);
+      } else {
+        console.error('API response is not an array:', data);
+        setQuery([]);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setQuery([]);
+    }
+  };
+
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <button onClick={fetchData}>Get Data</button>
+      <div>
+        <h2>Extracted Data:</h2>
+        {query.map((item, index) => (
+          <div key={index}>
+            <h3>{item.title}</h3>
+            <p>Link: {item.link}</p>
+            <p>Publication Date: {item.pubDate}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+}
